8e3c4d59484831e68cb0c748c9bba819e88f5be4
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-02-13 14:50:43 +0300

    DTO's and Core layer added.

---
+++ FinalProject/Business/Concrete/CategoryManager.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categaryDal;
+        public CategoryManager(ICategoryDal categaryDal)
+        {
+            _categaryDal = categaryDal;
+        }
+        public List<Category> GetAll()
+        {
+            return _categaryDal.GetAll();
+        }
+        //Select * from Categories where CategoryId = categoryId
+        public Category GetById(int categoryId)
+        {
+            return _categaryDal.Get(c => c.CategoryID == categoryId);
+        }
+    }
+}

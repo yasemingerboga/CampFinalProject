50148f14023b056c990430c5c86757ff129e322d
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-03-11 20:48:15 +0300

    bugfix using Core.Extensions

--- FinalProject/Core/Utilities/Security/JWT/JwtHelper.cs
+++ FinalProject/Core/Utilities/Security/JWT/JwtHelper.cs
@@ -1,5 +1,5 @@
 ﻿using Core.Entities.Concrete;
-using Core.Utilities.Extensions;
+using Core.Extensions;
 using Core.Utilities.Security.Encryption;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;

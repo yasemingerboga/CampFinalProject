a92e943c6b905bf391176c5d100be7ad0747ada9
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-04-04 00:54:55 +0300

    update

--- FinalProject/Business/ValidationRules/FluentValidation/ProductValidator.cs
+++ FinalProject/Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -15,7 +15,7 @@ namespace Business.ValidationRules.FluentValidation
             RuleFor(p => p.UnitPrice).NotEmpty();
             RuleFor(p => p.UnitPrice).GreaterThan(0);
             RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryID == 1);
-            RuleFor(p => p.ProductName).Must(StartWithA).WithMessage("Ürünler A ile başlamalı");
+            RuleFor(p => p.ProductName).Must(StartWithA).WithMessage("Products must start with 'A' .");
 
         }
 
